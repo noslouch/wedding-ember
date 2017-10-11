8e9e291fe22846bdc56762461077f19f024606d0
Brian Whitton <me@brianwhitton.com>
2017-10-10 21:32:01 -0400

    adds people and registry routes

--- app/routes/people.js
+++ app/routes/people.js
@@ -1,4 +1,55 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return {
+      maids: [{
+        name: 'Laura Esner',
+        image: '',
+      }, {
+        name: 'Katie Sachsenmeier Wang',
+        image: '',
+      }, {
+        name: 'Bevin Cohen',
+        image: '',
+      }, {
+        name: 'Maya Bradford',
+        image: '',
+      }, {
+        name: 'Caitlin Mae Burke',
+        image: ''
+      }, {
+        name: '',
+        image: ''
+      }, {
+        name: '',
+        image: ''
+      }],
+      men: [{
+        name: 'Brad Farberman',
+        image: ''
+      }, {
+        name: 'Dimitri Apostol',
+        image: '',
+      }, {
+        name: 'Konstantin Karmazin',
+        image: '',
+      }, {
+        name: 'Michael Capristo',
+        image: ''
+      }, {
+        name: 'Gabriel Herrera',
+        image: '',
+      }, {
+        name: 'Ian Lamb',
+        image: '',
+      }, {
+        name: 'Joe Ammon',
+        image: ''
+      }, {
+        name: 'Matt Chaban',
+        image: ''
+      }]
+    }
+  }
 });
