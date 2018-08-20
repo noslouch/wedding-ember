fba7e87c6877f087a7d0621491c3b32ffb166b9f
Brian Whitton <me@brianwhitton.com>
2018-08-20 00:50:37 -0400

    methods for components

--- app/controllers/rsvp.js
+++ app/controllers/rsvp.js
@@ -2,5 +2,17 @@ import Controller from '@ember/controller';
 import config from '../config/environment';
 
 export default Controller.extend({
-  showRSVP: config.RSVP
+  showRSVP: config.RSVP,
+
+  sendResponse(invitation) {
+    invitation.save()
+      .then(() => this.set('success', true));
+  },
+
+  reset() {
+    this.setProperties({
+      success: false,
+      invitation: null
+    })
+  }
 });
