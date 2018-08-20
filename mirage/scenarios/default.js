11f643561fa43da58678926890405c4f66b6d6bf
Brian Whitton <me@brianwhitton.com>
2018-08-20 00:49:56 -0400

    mirage default scnearios

--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -1,9 +1,12 @@
-export default function(/* server */) {
+export default function(server) {
 
   /*
     Seed your development database using your factories.
     This data will not be loaded in your tests.
   */
 
-  // server.createList('post', 10);
+  server.createList('invitation', 10, 'basic');
+  server.create('invitation', 'basic', {address: 'home'});
+
+  console.log(server.schema.guests.all().models.mapBy('firstName')); // eslint-disable-line
 }
