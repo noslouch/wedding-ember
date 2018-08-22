f09a1789b46c681cac683ec6bf4ebd2248dfa72a
Brian Whitton <me@brianwhitton.com>
2018-08-22 00:23:36 -0400

    create some basic data types

--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -5,8 +5,13 @@ export default function(server) {
     This data will not be loaded in your tests.
   */
 
-  server.createList('invitation', 10, 'basic');
-  server.create('invitation', 'basic', {address: 'home'});
+  server.create('invitation', 'basic', {id: 'basic'});
+  server.create('invitation', 'bothAttending', {id: 'attending'});
+  server.create('invitation', 'bothBrunch', {id: 'brunch'});
+  server.create('invitation', 'bothRehearsal', {id: 'rehearsal'});
+  server.create('invitation', 'bothAll', {id: 'all'});
+  server.create('invitation', 'includePlusOne', {id: 'with-plus-one'});
+  server.create('invitation', 'plusOneInvite', {id: 'plus-one'});
 
   console.log(server.schema.guests.all().models.mapBy('firstName')); // eslint-disable-line
 }
