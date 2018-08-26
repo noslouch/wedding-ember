80a8078b147ad9fb0476c235eec5ff1080f33105
Brian Whitton <me@brianwhitton.com>
2018-08-25 23:28:11 -0400

    new guest detail card components

--- app/components/guest-detail/component.js
+++ app/components/guest-detail/component.js
@@ -3,6 +3,8 @@ import Component from '@ember/component';
 export default Component.extend({
   classNames: ['guest-detail'],
 
+  classNameBindings: ['name'],
+
   'data-test-guest-detail': true,
 
   canEditName: false
--- app/components/guest-detail/name/component.js
+++ app/components/guest-detail/name/component.js
@@ -2,5 +2,11 @@ import Component from '@ember/component';
 
 export default Component.extend({
   classNames: ['guest-name'],
-  classNameBindings: ['canEditName:is-editing'],
+  classNameBindings: ['isEditing:is-editing', 'guest.isPlusOne:is-plus-one'],
+
+  edit() {
+    if (this.guest.isPlusOne) {
+      this.set('isEditing', true);
+    }
+  }
 });
