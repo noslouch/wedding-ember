c8d8313eae2ff069e4c2daf3fe459c5ba9f98251
Brian Whitton <me@brianwhitton.com>
2018-08-26 00:27:59 -0400

    add brunch info component

---
+++ app/components/brunch-info/component.js
@@ -0,0 +1,17 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+  tagName: 'span',
+  classNames: ['brunch-info'],
+
+  calculatePosition(trigger, content) {
+  let { top, left, height, width } = trigger.getBoundingClientRect();
+  let { height: contentHeight, width: contentWidth } = content.getBoundingClientRect();
+  let style = {
+    top: window.pageYOffset + ( top - height - contentHeight - 10),
+    left: left - ((contentWidth / 2) - (width / 2)),
+  };
+
+  return { style };
+}
+});
