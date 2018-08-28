b8053eed10d1d7b21cda99106ec73220b5f37da7
Brian Whitton <me@brianwhitton.com>
2018-08-27 23:00:57 -0400

    skip second line if it doesn't have a comma

--- app/helpers/slice-address.js
+++ app/helpers/slice-address.js
@@ -1,8 +1,12 @@
 import { helper } from '@ember/component/helper';
 
 export function sliceAddress([ address = '']/*, hash*/) {
-  let [name, , city] = address.split('\n')
-  return [name, city];
+  let [name, aptOrStreet, streetOrCity] = address.split('\n');
+  if (/,/.test(aptOrStreet)) {
+    return [name, aptOrStreet];
+  } else {
+    return [name, streetOrCity];
+  }
 }
 
 export default helper(sliceAddress);
