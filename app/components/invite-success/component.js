3f3002297c02a4e6e59786e6759088e150f5127e
Brian Whitton <me@brianwhitton.com>
2018-08-31 00:15:26 -0400

    update success screen

--- app/components/invite-success/component.js
+++ app/components/invite-success/component.js
@@ -1,9 +1,13 @@
 import Component from '@ember/component';
+import { filterBy, setDiff } from '@ember/object/computed';
 
 export default Component.extend({
   classNames: ['invite-success'],
 
   'data-test-rsvp-success': true,
 
-  reset() {}
+  reset() {},
+
+  attending: filterBy('invitation.guests', 'weddingRsvp', true),
+  notAttending: setDiff('invitation.guests', 'attending'),
 });
